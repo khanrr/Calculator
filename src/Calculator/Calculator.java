298d5378b22b8092eaaececd14b0bcc04d2c592b
khanr <khanrr11@gmail.com>
2025-09-02 14:47:28 -0400

    First commit

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,48 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double number1, number2, result;
+
+        System.out.println("Enter first number: ");
+        number1 = scanner.nextDouble();
+
+        System.out.println("Enter operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.println("Enter second number: ");
+        number2 = scanner.nextDouble();
+
+        switch (operator){
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("invalid operator entered!");
+                break;
+        }
+
+        scanner.close();
+
+    }
+}
